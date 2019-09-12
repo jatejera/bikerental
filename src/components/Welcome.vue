f613b7b54d8918dafda05bebba1a91db42417f96
Julian Tejera <jats.tejera@gmail.com>
2019-09-12 09:09:03 -0400

    small changes

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,15 +3,26 @@
     <div>
       <img class="img-fluid" alt="Vue logo" src="../assets/logo.png" />
     </div>
-    <h1 class="display-4"> Welcome to <br />SampleApp Bike Rental </h1>
-    <p class="lead"> We have an amazing collection of bikes for rent. Your satisfaction is very important to us. Click
-      below to browse our not-so-large collection of bikes </p>
-    <router-link to="/rent-a-bike" class="btn btn-outline-primary align-self-center mb-3">Rent-A-Bike</router-link>
-    <small class="text-muted">This App was developed by Julian Tejera using Vue.js. jats.tejera@gmail.com. Click <a href="/download" target="_blank" rel="noopener noreferrer">HERE</a> to download this app </small>
+    <h1 class="display-4">
+      Welcome to
+      <br />TopView Bike Rental
+    </h1>
+    <p class="lead">
+      We have an amazing collection of bikes for rent. Your satisfaction is very important to us. Click
+      below to browse our not-so-large collection of bikes
+    </p>
+    <router-link
+      to="/rent-a-bike"
+      class="btn btn-outline-primary align-self-center mb-3"
+    >Rent-A-Bike</router-link>
+    <small class="text-muted">
+      This App was developed by Julian Tejera using Vue.js. jats.tejera@gmail.com. Click
+      <a href="/download" target="_blank" rel="noopener noreferrer">HERE</a> to download this app
+    </small>
   </div>
 </template>
 <script>
-  export default {
-    name: "Welcome"
-  };
+export default {
+  name: "Welcome"
+};
 </script>
